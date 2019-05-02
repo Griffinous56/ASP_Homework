c7dd737e141fa10336c4045268f57b7e1e5a9afd
TaiPhan <bombom18699@gmail.com>
2019-05-02 15:37:47 +0700

    update

--- WebApplication1/WebApplication1.Tests/Controllers/VLTeaTest.cs
+++ WebApplication1/WebApplication1.Tests/Controllers/VLTeaTest.cs
@@ -7,6 +7,7 @@ using WebApplication1.Tests;
 using System.Web.Mvc;
 using WebApplication1.Controllers;
 using System.Collections.Generic;
+using System.Transactions;
 
 namespace WebApplication1.Tests.Controllers
 {
@@ -25,6 +26,99 @@ namespace WebApplication1.Tests.Controllers
             var model = view.Model as List<BubleTea>;
             Assert.IsNotNull(model);
             Assert.AreEqual(db.BubleTeas.Count(), model.Count);
+        }
+
+        [TestMethod]
+        public void testCreateG()
+        {
+            var controller = new VLTeaController();
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void testCreateP()
+        {
+            var model = new BubleTea
+            {
+                Name = "Bông cúc",
+                Topping = "trân châu, bánh flan",
+                Price = 0
+            };
+
+            var db = new CS4PEEntities();
+            var controller = new VLTeaController();
+
+            using (var scope = new TransactionScope())
+            {
+                var result = controller.Create(model);
+                var view = result as ViewResult;
+                Assert.IsNotNull(view);
+                Assert.IsInstanceOfType(view.Model, typeof(BubleTea));
+                Assert.AreEqual(Resource1.PRICE_LESS_0,
+                    controller.ViewData.ModelState["Price"].Errors[0].ErrorMessage);
+
+                model.Price = 20000;
+                controller = new VLTeaController();
+                result = controller.Create(model);
+                var redirect = result as RedirectToRouteResult;
+                Assert.IsNotNull(redirect);
+                Assert.AreEqual("Index", redirect.RouteValues["action"]);
+                var item = db.BubleTeas.Find(model.id);
+                Assert.IsNotNull(item);
+                Assert.AreEqual(model.Name, item.Name);
+                Assert.AreEqual(model.Topping, item.Topping);
+                Assert.AreEqual(model.Price, item.Price);
+            }
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            var db = new CS4PEEntities();
+            var controller = new VLTeaController();
+
+            var result = controller.Delete(0);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+
+            using (var scope = new TransactionScope())
+            {
+                var model = db.BubleTeas.AsNoTracking().First();
+                result = controller.Delete(model.id);
+                var redirect = result as RedirectToRouteResult;
+                Assert.IsNotNull(redirect);
+                Assert.AreEqual("Index", redirect.RouteValues["action"]);
+                var item = db.BubleTeas.Find(model.id);
+                Assert.IsNull(item);
+            }
+        }
+
+        [TestMethod]
+        public void testEditP()
+        {
+            var controller = new VLTeaController();
+            {
+                var db = new CS4PEEntities();
+                var result = controller.Edit(0);
+                Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+                var item = db.BubleTeas.First();
+                var kq = controller.Edit(item.id) as ViewResult;
+                Assert.IsNotNull(kq);
+                var model = kq.Model as BubleTea;
+                Assert.AreEqual(item.id, model.id);
+            }
+        }
+
+        [TestMethod]
+        public void TestEditG()
+        {
+            var db = new CS4PEEntities();
+            var controller = new VLTeaController();
+
+            var result = controller.Edit(0);
+            Assert.IsInstanceOfType(result, typeof(HttpNotFoundResult));
+
+            var item = db.BubleTeas.First().id;
         }
     }
 }
--- WebApplication1/WebApplication1/Controllers/VLTeaController.cs
+++ WebApplication1/WebApplication1/Controllers/VLTeaController.cs
@@ -18,6 +18,61 @@ namespace WebApplication1.Controllers
             return View(model.ToList());
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(BubleTea model)
+        {
+            ValidateBubbleTea(model);
+            if (ModelState.IsValid)
+            {
+                db.BubleTeas.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        private void ValidateBubbleTea(BubleTea model)
+        {
+            if (model.Price <= 0)
+            {
+                ModelState.AddModelError("Price", Resource1.PRICE_LESS_0);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var model = db.BubleTeas.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            db.BubleTeas.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = db.BubleTeas.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(BubleTea model)
+        {
+            ValidateBubbleTea(model);
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = System.Data.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
