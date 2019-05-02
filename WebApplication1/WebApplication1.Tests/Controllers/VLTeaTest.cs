debfeeb553dd722fe3856ba035e89e55adffca04
TaiPhan <bombom18699@gmail.com>
2019-05-02 13:52:45 +0700

    Update

---
+++ WebApplication1/WebApplication1.Tests/Controllers/VLTeaTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using WebApplication1.Models;
+using WebApplication1.Tests.Controllers;
+using WebApplication1.Tests;
+using System.Web.Mvc;
+using WebApplication1.Controllers;
+using System.Collections.Generic;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class VLTeaTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new CS4PEEntities();
+            var controller = new VLTeaController();
+            var result = controller.Index();
+
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+            var model = view.Model as List<BubleTea>;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(db.BubleTeas.Count(), model.Count);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/VLTeaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class VLTeaController : Controller
+    {
+        //
+        // GET: /VLTea/
+        CS4PEEntities db = new CS4PEEntities();
+        public ActionResult Index()
+        {
+            var model = db.BubleTeas;
+            return View(model.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+	}
+}
